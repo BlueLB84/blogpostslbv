694a9981cf353423433f1b1395aac68fe65b1300
L Barbee <bluelb84@gmail.com>
2017-09-19 22:02:13 -0400

    setup of router and server.js

---
+++ blogPostRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('bodyParser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+
+BlogPosts.create('Blog Post 1', 'This is the first blog post.', 'LBV');
+BlogPosts.create('Blog Post 2', 'This is the second blog post.', 'MBV');
+
+router.get('/', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	const postItem = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(postItem); 
+	});
+
+router.delete('/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted blog entry \`${req.params.id}\``);
+	res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'id'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+	if (req.params.id !== req.body.id) {
+		const message = (`Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);
+		console.error(message);
+		return res.status(400).send(message);
+	}
+	console.log(`Updating blog entry \`${req.params.id}\``);
+	const updatedBlogPost = BlogPosts.update({
+		id: req.params.id,
+		title: req.params.title,
+		content: req.params.content,
+		author: req.params.author,
+		publishDate: req.params.publishDate
+	});
+	res.status(204).end();
+});
+
+module.exports = router;
